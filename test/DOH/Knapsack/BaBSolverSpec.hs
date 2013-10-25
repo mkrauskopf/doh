542a415aaad3e103687c961a697fe29d322c0e8b
Martin Krauskopf <martin.krauskopf@gmail.com>
2013-10-25 08:41:02 +0200

    Add some harder problem to tests
    Which shows that current implementation is far from optimal.

--- test/DOH/Knapsack/BaBSolverSpec.hs
+++ test/DOH/Knapsack/BaBSolverSpec.hs
@@ -3,14 +3,15 @@ module DOH.Knapsack.BaBSolverSpec (main, spec) where
 import Test.Hspec.HUnit ()
 import Test.HUnit hiding (path)
 import Test.Hspec
-import DOH.Knapsack.BaBSolver (solve, debugSolve)
+import DOH.Knapsack.BaBSolver (solve, debugSolve, Item)
+import Control.Applicative((<$>))
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
-  describe "Branch and Bound algorithm knapsack using" $ do
+spec = do
+  describe "BaB, simple problems, using" $ do
     it "zero items" $
       (0, []) @=? debugSolve 10 []
 
@@ -33,4 +34,25 @@ spec =
     it "three items 2" $ do
       let items = [ (45, 5), (48, 8), (35, 3) ]
       (7, [True,False,True]) @=? debugSolve 10 items
+
+  describe "BaB, harder problems, using" $ do
+    -- TODO: far from optimal
+    it "19 items" $ assertValue "test/data/ks_19_0" 11555 -- optimal: 12248
+    it "30 items" $ assertValue "test/data/ks_30_0" 90000 -- optimal: 99798
+
+
+assertValue :: String -> Double -> Assertion
+assertValue path optVal = do
+    (_, val) <- uncurry solve . processFile <$> readFile path
+    optVal @=? val
+
+
+-- | Parses sample knapsack file
+processFile :: String -> (Integer, [Item])
+processFile content = (cap, items)
+  where ls = lines content
+        cap = read . (!! 1) . words $ head ls
+        items = map (tuplify2 . words) (tail ls)
+        tuplify2 [v,w] = (read v, read w)
+        tuplify2 _ = error "tuplify2 must be passed two-elements list"
 
